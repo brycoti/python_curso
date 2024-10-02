0784c7d6f0313b3e400c5b8bcbb4c9ee5af43694
brycoti <bryancozartitos@gmail.com>
2024-10-02 16:35:07 +0200

    comentarios en py

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,11 @@
+# Comentar linea en py con un #
+
+print("Hola")
+# print("mundo")
+
+# """ para hacer comentarios multilinea """
+
+"""
+print("Esto no se va a mostrar ")
+print ("en un comentario multilinea")    
+"""
